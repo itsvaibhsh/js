284451d1ea966ac53fda0090cd1681a07ce12459
itsvaibhsh <147572816+itsvaibhsh@users.noreply.github.com>
2023-10-12 01:50:46 +0000

    variables and datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict" //treat all JS code as newer version
+
+// alert ( 3+3) // we are using nodejs, not browser
+
+console.log( 3+3)
+console.log("Vaibhav")
+
+let name ="hitesh"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined
+//symbol => unique
+
+//object
+
+console.log(typeof "hitesh");
+console.log(typeof age);
+console.log(typeof null);    // object
+console.log(typeof undefined);  //undefined
